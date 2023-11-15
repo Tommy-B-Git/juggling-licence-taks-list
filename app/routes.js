99189405e3148274eab16ee6851d3fba9529a7e9
Tom <thomas.h.brown@icloud.com>
2023-11-15 17:23:05 +0000

    Added app/views/can-juggle.html         app/views/code.html         app/views/email-address.html         app/views/has-account.html         app/views/not-eligible.html         app/views/start.html

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,13 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+// EMAIL /////////
+
+router.post('/has-account', (req, res) => {
+    if (req.session.data.new.hasAccount == 'No') {
+        res.redirect('/can-juggle')
+    } else {
+        res.redirect('/email-address')
+    }
+})
