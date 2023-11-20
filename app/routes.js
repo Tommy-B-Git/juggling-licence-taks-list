a9b0b67c97543a7e1c767a67d9e9ac9d1246f25c
Tom <thomas.h.brown@icloud.com>
2023-11-20 12:57:03 +0000

    added email-addess code hass-account not-eligible Start.html and all associate routes

--- app/routes.js
+++ app/routes.js
@@ -6,14 +6,33 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-// Add your routes here
 
-// EMAIL /////////
-
+/////////// HAS ACCOUNT /////////
 router.post('/has-account', (req, res) => {
     if (req.session.data.new.hasAccount == 'No') {
         res.redirect('/can-juggle')
     } else {
         res.redirect('/email-address')
     }
+})
+
+
+/////////// CAN JUGGLE /////////
+router.post('/can-juggle', (req, res) => {
+    if (req.session.data.new.canJuggle == 'No') {
+        res.redirect('/not-eligible')
+    } else {
+        res.redirect('/email-address')
+    }
+})
+
+
+/////////// EMAIL OK /////////
+router.post('/email-address', (req, res) => {
+    res.redirect('/code')
+})
+
+/////////// CODE OK /////////
+router.post('/code', (req, res) => {
+    res.redirect('/application')
 })
