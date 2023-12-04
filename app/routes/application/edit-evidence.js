c63e01fafd63186b493bfe84e1d575a199ab03e1
Tom <thomas.h.brown@icloud.com>
2023-12-04 14:35:04 +0000

    Added upload logic for files + lodash and uuid generator

--- app/routes/application/edit-evidence.js
+++ app/routes/application/edit-evidence.js
@@ -1,3 +1,6 @@
+const _ = require('lodash')
+const { v4 : uuidv4 } = require('uuid')
+
 module.exports = router => {
 
     router.post('/application/edit-evidence/has-evidence', (req, res) => {
@@ -9,6 +12,29 @@ module.exports = router => {
     })
 
     router.post('/application/edit-evidence/upload', (req, res) => {
+
+        let files = [
+            'trick-performance.mp4',
+            'juggling-show.mp4',
+            'testimonial.mp3'
+        ]
+
+        
+        if (!req.session.data.evidence.files) {
+            req.session.data.evidence.files = {}
+        }
+        // get the next file
+        let filesCount = _.size(req.session.data.evidence.files) // 0
+        let nextFile = files[filesCount]
+        
+        // Store next file in memory to use later
+        if (nextFile) {
+            req.session.data.evidence.files[uuidv4()] = {
+                filename: nextFile
+            }    
+        }
+        
+
         res.redirect('/application/edit-evidence/check-files')  
     })
 
